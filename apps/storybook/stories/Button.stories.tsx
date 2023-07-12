67804f0f8241b33ec7975455d23acb7d0fa8e381
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-07-12 11:47:39 +0200

    fix workspace symbolic link dependencies

---
+++ apps/storybook/stories/Button.stories.tsx
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '@lonlat/components/button/Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  parameters: {
+      
+  },
+  // tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    primary: true,
+    children: 'Button',
+  },
+};
+
+export const Test = () => (
+  <Button>Coucou</Button>
+)
+
+export const Secondary: Story = {
+  args: {
+    children: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    children: 'Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+    children: 'Button',
+  },
+};
--- apps/vite-sandbox/vite.config.ts
+++ apps/vite-sandbox/vite.config.ts
@@ -1,17 +1,7 @@
-import { resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-const projectDir = resolve(fileURLToPath(new URL("../..", import.meta.url)));
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@lonlat/components": resolve(projectDir, 'libs/components')
-    }
-  }
-})
+});
