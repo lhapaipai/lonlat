adeb76337be09ab774fba03e7d18fb33a515ce28
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-07-27 23:59:07 +0200

    update storybook favicon

--- apps/storybook/.storybook/theme.ts
+++ apps/storybook/.storybook/theme.ts
@@ -3,5 +3,12 @@ import { create } from "@storybook/theming/create";
 // complete options
 // https://storybook.js.org/docs/react/configure/theming#create-a-theme-quickstart
 export default create({
-  base: 'light'
+  base: 'light',
+  colorPrimary: '#dba726',
+  colorSecondary: '#dba726',
+
+  appBorderRadius: 6,
+  brandTitle: 'LonLat',
+  brandImage: '/logo.svg',
+  brandUrl: '/'
 })
