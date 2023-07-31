3bc4062fd88b785837f7131af96b8c77747dd212
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-07-31 18:15:19 +0200

    add LLMarker basic implementation

--- apps/storybook/stories/maplibre-ext/LLMarker.stories.tsx
+++ apps/storybook/stories/maplibre-ext/LLMarker.stories.tsx
@@ -1,10 +1,163 @@
 import { useEffect, useRef } from "react";
 import * as maplibre from "maplibre-gl";
 import { LLMarker } from "@lonlat/maplibre-ext";
+import "maplibre-gl/dist/maplibre-gl.css";
+
 const meta = {
   title: "Maplibre-ext/LLMarker",
 };
 export default meta;
+
+export const Graphics = () => (
+  <div>
+    <div className="storybook-icon-grid">
+      <div>
+        <div className="ll-marker">
+          {/* i is wrapped so we can apply transform when active */}
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-star"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="storybook-icon-grid">
+      <div>
+        <div className="ll-marker">
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-road"></i>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="ll-marker selected">
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-heart"></i>
+          </div>
+        </div>
+        <pre>.selected</pre>
+      </div>
+    </div>
+
+    {/* <div className="storybook-icon-grid">
+      <div className="ll-marker marker-cluster">
+        <div>
+          <i className="fe-cluster-station-hyd"></i>
+          <div className="text">5</div>
+        </div>
+      </div>
+      <div className="ll-marker marker-cluster">
+        <div>
+          <i className="fe-cluster-station-cam"></i>
+          <div className="text">15</div>
+        </div>
+      </div>
+      <div className="ll-marker marker-cluster">
+        <div>
+          <i className="fe-cluster-station-met"></i>
+          <div className="text">25</div>
+        </div>
+      </div>
+      <div className="ll-marker marker-cluster">
+        <div>
+          <i className="fe-cluster-station-nph"></i>
+          <div className="text">105</div>
+        </div>
+      </div>
+      <div className="ll-marker marker-cluster">
+        <div>
+          <i className="fe-cluster-station-mf"></i>
+          <div className="text">225</div>
+        </div>
+      </div>
+    </div> */}
+
+    <div className="storybook-icon-grid">
+      <div>
+        <div className="ll-marker disabled">
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-housenumber"></i>
+          </div>
+        </div>
+        <pre>.disabled</pre>
+      </div>
+      <div>
+        <div className="ll-marker">
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-town"></i>
+            <div className="inactive"></div>
+          </div>
+        </div>
+        <pre>.inactive</pre>
+      </div>
+    </div>
+
+    <div className="storybook-icon-grid">
+      <div>
+        <div className="ll-marker" style={{ "--marker-size": "30px" }}>
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-route"></i>
+          </div>
+        </div>
+        <pre>--marker-size: 30px</pre>
+      </div>
+      <div>
+        <div className="ll-marker" style={{ "--marker-size": "50px" }}>
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-municipality"></i>
+          </div>
+        </div>
+        <pre>--marker-size: 50px</pre>
+      </div>
+      <div>
+        <div className="ll-marker" style={{ "--marker-size": "70px" }}>
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-home"></i>
+          </div>
+        </div>
+        <pre>--marker-size: 70px</pre>
+      </div>
+      <div>
+        <div className="ll-marker" style={{ "--marker-size": "90px" }}>
+          <div>
+            <div className="ovale"></div>
+            <i className="fe-climbing-outdoor"></i>
+          </div>
+        </div>
+        <pre>--marker-size: 90px</pre>
+      </div>
+    </div>
+
+    {/* <div className="storybook-icon-grid">
+      <div className="ll-marker marker-cluster" style={{ "--marker-size": "44px" }}>
+        <div>
+          <i className="fe-cluster-station-met"></i>
+          <div className="text">25</div>
+        </div>
+      </div>
+      <div className="ll-marker marker-cluster" style={{ "--marker-size": "60px" }}>
+        <div>
+          <i className="fe-cluster-station-nph"></i>
+          <div className="text">105</div>
+        </div>
+      </div>
+      <div className="ll-marker marker-cluster" style={{ "--marker-size": "80px" }}>
+        <div>
+          <i className="fe-cluster-station-mf"></i>
+          <div className="text">225</div>
+        </div>
+      </div>
+    </div> */}
+  </div>
+);
 
 export const Basic = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -16,7 +169,12 @@ export const Basic = () => {
       zoom: 4,
     });
 
-    new LLMarker().setLngLat([5, 45]).addTo(map);
+    new LLMarker({
+      draggable: true,
+    })
+      .setLngLat([-1.1344, 44.698])
+      .addTo(map);
+    // new maplibre.Marker().setLngLat([-1.1344, 44.698]).addTo(map);
   }, []);
   return (
     <div ref={containerRef} style={{ height: "var(--storybook-preview-viewport-height)" }}></div>
